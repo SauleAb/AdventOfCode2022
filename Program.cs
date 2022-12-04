50ab47201f37cae67ee20473eea26cf6974147f8
SauleAb <abramoviciutesaule@gmail.com>
2022-12-04 22:20:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿void Day1()
+{
+    string[] match = File.ReadAllLines("Day2.txt");
+
+
+    int count = 0;
+    int rock = 1; //rock
+    int paper = 2; //paper
+    int scissors = 3; //scissors
+
+
+foreach (string line in match)
+{
+        if (line[0] == 'A')
+        {
+            if (line[2] == 'X')
+                count = count + scissors + 0;
+            if (line[2] == 'Y')
+                count = count + rock + 3;
+            if (line[2] == 'Z')
+                count = count + paper + 6;
+        }
+
+        if (line[0] == 'B')
+        {
+            if (line[2] == 'X')
+                count = count + rock + 0;
+            if (line[2] == 'Y')
+                count = count + paper + 3;
+            if (line[2] == 'Z')
+                count = count + scissors + 6;
+        }
+
+        if (line[0] == 'C')
+        {
+            if (line[2] == 'X')
+                count = count + paper + 0;
+            if (line[2] == 'Y')
+                count = count + scissors + 3;
+            if (line[2] == 'Z')
+                count = count + rock + 6;
+        }
+    }
+
+    Console.WriteLine(count);
+}
+

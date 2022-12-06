dcca36e6c6c3f7f84b5081ab3926530acb2793aa
SauleAb <abramoviciutesaule@gmail.com>
2022-12-06 23:51:10 +0200

    Day3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿void Day1()
+﻿using System.ComponentModel;
+
+void Day2()
 {
     string[] match = File.ReadAllLines("Day2.txt");
 
@@ -7,7 +9,7 @@
     int paper = 2;
     int scissors = 3;
 
-foreach (string line in match)
+    foreach (string line in match)
 {
         if (line[0] == 'A')
         {
@@ -43,3 +45,72 @@ foreach (string line in match)
     Console.WriteLine(count);
 }
 
+void Day3()
+{
+    string[] items = File.ReadAllLines("Day3.txt");
+    string compartment1;
+    string compartment2;
+    int count = 0;
+    int position = 0;
+    foreach (string item in items)
+    {
+        count = item.Count() / 2;
+        compartment1 = item.Substring(0, count);
+        compartment2 = item.Substring(count);
+        foreach (char c in compartment1)
+        {
+            if (compartment2.Contains(c))
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    position += Math.Abs(c - 'a' + 1);
+                    break;
+                }
+                else
+                {
+                    position += Math.Abs(c - 'A' + 27);
+                    break;
+                }
+            }
+        }
+    }
+    Console.WriteLine(position);
+}
+
+void Day3Part2()
+{
+    string[] items = File.ReadAllLines("Day3.txt");
+    string first;
+    string second;
+    string third;
+    int f=0;
+    int s=1;
+    int t=2;
+    int position = 0;
+    for (int i=0; i<items.Length; i++)
+    {
+        first = items[f];
+        second = items[s];
+        third = items[t];
+        foreach (char c in first)
+        {
+            if (second.Contains(c) && third.Contains(c))
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    position += Math.Abs(c - 'a' + 1);
+                    break;
+                }
+                else
+                {
+                    position += Math.Abs(c - 'A' + 27);
+                    break;
+                }
+            }
+        }
+        f += 3;
+        s += 3;
+        t += 3;
+    }
+    Console.WriteLine(position);
+}
